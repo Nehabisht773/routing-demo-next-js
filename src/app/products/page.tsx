86abbc150d5403ad4a8715caf9e94c4562ee85aa
Nehabisht773 <nehbisht@deloitte.com>
2024-04-09 18:02:16 +0530

    dynamic routing , nested routing and catch all segments

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductList() {
+    return (
+        <>
+        <h1>Product List 1</h1>
+        <h1>Product List 2</h1>
+        <h1>Product List 3</h1>     
+        </>
+    )
+}
